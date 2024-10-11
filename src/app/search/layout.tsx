447faa578caf4e94489bdff2e00b38f12ca2aefd
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2024-10-11 14:20:03 +0500

    wrap suspense to search page

--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Suspense } from "react";
 
 
 export const metadata: Metadata = {
@@ -12,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <Suspense>
         {children}
-    </>
+    </Suspense>
   );
 }
